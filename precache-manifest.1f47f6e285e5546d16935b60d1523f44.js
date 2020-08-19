690fde9c324f0f627a27388b3ea443012ea69c64
lorebook <contactshanitiwari@gmail.com>
2020-08-19 21:41:10 +0530

    Updates

---
+++ precache-manifest.1f47f6e285e5546d16935b60d1523f44.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa18ded8242cc32b820cf21b9f549710",
+    "url": "/arovez-technology/index.html"
+  },
+  {
+    "revision": "c3428fd01c21eb54bf6f",
+    "url": "/arovez-technology/static/css/1.526b8a64.chunk.css"
+  },
+  {
+    "revision": "a768a61ba3b663c04128",
+    "url": "/arovez-technology/static/css/4.853b5499.chunk.css"
+  },
+  {
+    "revision": "06e5aa500335aae109dd",
+    "url": "/arovez-technology/static/css/5.0e5150c2.chunk.css"
+  },
+  {
+    "revision": "6e28642dce1cd5bdc550",
+    "url": "/arovez-technology/static/css/8.ffcf5008.chunk.css"
+  },
+  {
+    "revision": "f6acc624350001300c09",
+    "url": "/arovez-technology/static/js/0.9ac520d7.chunk.js"
+  },
+  {
+    "revision": "c3428fd01c21eb54bf6f",
+    "url": "/arovez-technology/static/js/1.2937c807.chunk.js"
+  },
+  {
+    "revision": "559b5a6ebf1080973fb8",
+    "url": "/arovez-technology/static/js/10.b29dcb57.chunk.js"
+  },
+  {
+    "revision": "3a36438ff70bc4aa5b99",
+    "url": "/arovez-technology/static/js/11.65a157ab.chunk.js"
+  },
+  {
+    "revision": "c8b1baca1f8bbe8372c0",
+    "url": "/arovez-technology/static/js/12.0ec6c400.chunk.js"
+  },
+  {
+    "revision": "26e98a67817974049f03",
+    "url": "/arovez-technology/static/js/13.8687e7de.chunk.js"
+  },
+  {
+    "revision": "0108a6e6c1e5611334ec",
+    "url": "/arovez-technology/static/js/14.be788d95.chunk.js"
+  },
+  {
+    "revision": "da9c864f9529bb484b3d",
+    "url": "/arovez-technology/static/js/15.dd9ca12a.chunk.js"
+  },
+  {
+    "revision": "14936d98a4132cf6202f",
+    "url": "/arovez-technology/static/js/16.63ec84de.chunk.js"
+  },
+  {
+    "revision": "15da4462833af9f04043",
+    "url": "/arovez-technology/static/js/17.9d093986.chunk.js"
+  },
+  {
+    "revision": "99572e61e71690bbce89",
+    "url": "/arovez-technology/static/js/18.d34cac12.chunk.js"
+  },
+  {
+    "revision": "033a80df65d22715c299",
+    "url": "/arovez-technology/static/js/19.75298241.chunk.js"
+  },
+  {
+    "revision": "22d2624286fe73b4e7cb",
+    "url": "/arovez-technology/static/js/20.d87792e0.chunk.js"
+  },
+  {
+    "revision": "dfea67b4d1e980841010",
+    "url": "/arovez-technology/static/js/21.f10c9138.chunk.js"
+  },
+  {
+    "revision": "11fb79dc10a799154d69",
+    "url": "/arovez-technology/static/js/22.5071ae51.chunk.js"
+  },
+  {
+    "revision": "46316437b5f2ac2b1178",
+    "url": "/arovez-technology/static/js/23.25abf1e1.chunk.js"
+  },
+  {
+    "revision": "020b200c56ae19e748ca",
+    "url": "/arovez-technology/static/js/24.88c1bc90.chunk.js"
+  },
+  {
+    "revision": "d312be0daf1af03c0096",
+    "url": "/arovez-technology/static/js/25.882adf6b.chunk.js"
+  },
+  {
+    "revision": "3cb9de0b6f09ffa1ac6c",
+    "url": "/arovez-technology/static/js/26.c3192139.chunk.js"
+  },
+  {
+    "revision": "53f425c4e8c9f8bd9ddc",
+    "url": "/arovez-technology/static/js/27.c8a8aa4e.chunk.js"
+  },
+  {
+    "revision": "392639d1cc18bcdb524e",
+    "url": "/arovez-technology/static/js/28.916d3c0a.chunk.js"
+  },
+  {
+    "revision": "a768a61ba3b663c04128",
+    "url": "/arovez-technology/static/js/4.b461db00.chunk.js"
+  },
+  {
+    "revision": "06e5aa500335aae109dd",
+    "url": "/arovez-technology/static/js/5.3e2f3a97.chunk.js"
+  },
+  {
+    "revision": "6929369f1939f1d99f8c",
+    "url": "/arovez-technology/static/js/6.7b38edb1.chunk.js"
+  },
+  {
+    "revision": "b921562b380584ec983a",
+    "url": "/arovez-technology/static/js/7.b60addae.chunk.js"
+  },
+  {
+    "revision": "6e28642dce1cd5bdc550",
+    "url": "/arovez-technology/static/js/8.aad129ab.chunk.js"
+  },
+  {
+    "revision": "161456ed60fc4c3cf3a7",
+    "url": "/arovez-technology/static/js/9.6164dfa6.chunk.js"
+  },
+  {
+    "revision": "b033d61ba5629d6891a3",
+    "url": "/arovez-technology/static/js/main.a5281e7e.chunk.js"
+  },
+  {
+    "revision": "5b4bfa3abba5e5ce7a71",
+    "url": "/arovez-technology/static/js/runtime~main.c2223521.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/arovez-technology/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/arovez-technology/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/arovez-technology/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/arovez-technology/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/arovez-technology/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
